141c16f137c35db628537101bddc51801eaca38d
UrbanKocmut <u.kocmut@gmail.com>
2017-08-17 23:42:27 +0200

    Improved random generator and site visuals

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 "use strict";
 
 function getRandomInt(min, max) {
+
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    if (window.crypto !== undefined) {
+        var randomNum = window.crypto.getRandomValues(new Uint32Array(1))[0];
+        randomNum %= max;
+        randomNum -= min;
+        return randomNum;
+    } else {
+        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    }
+
 }
 
 function generateRandomCharacter() { //65-upercase 97-lowercase
@@ -22,3 +31,5 @@ function generateSeed() {
 function displaySeed() {
     document.getElementById("heading").innerText = generateSeed();
 }
+
+window.onload = displaySeed();
